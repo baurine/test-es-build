1fd8014bf1394c238239f9fa3ca74448dea429fd
baurine <2008.hbl@gmail.com>
2020-05-22 11:34:40 +0800

    use esbuild-loader

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,36 @@
-const { override, addBundleVisualizer } = require('customize-cra')
+const {
+  override,
+  addBundleVisualizer,
+  addWebpackPlugin,
+} = require('customize-cra')
+const { ESBuildPlugin } = require('esbuild-loader')
 
-module.exports = override(addBundleVisualizer())
+const useEsBuild = () => (config) => {
+  const rules = config.module.rules.find((rule) => Array.isArray(rule.oneOf))
+    .oneOf
+  const jsRule = rules.find(
+    (rule) =>
+      rule.loader &&
+      rule.loader.endsWith('babel-loader/lib/index.js') &&
+      rule.include
+  )
+  if (jsRule) {
+    jsRule.loader = 'esbuild-loader'
+    jsRule.options = {
+      // ...jsRule.options,
+      // All options are optional
+      target: 'es2015', // default, or 'es20XX', 'esnext'
+      jsxFactory: 'React.createElement',
+      jsxFragment: 'React.Fragment',
+      sourceMap: false, // Enable sourcemap
+    }
+    addWebpackPlugin(new ESBuildPlugin())(config)
+  }
+
+  return config
+}
+
+module.exports = override(
+  // addBundleVisualizer(),
+  useEsBuild(),
+)
